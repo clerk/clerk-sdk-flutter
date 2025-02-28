8a64151db0ec6729e90d7fd1c5d8efe40d2c0c36
Nic Ford <nicford@devangels.london>
2025-02-28 15:09:29 +0000

    fix: add tests [#142]

--- packages/clerk_flutter/lib/src/widgets/ui/clerk_loading_overlay.dart
+++ packages/clerk_flutter/lib/src/widgets/ui/clerk_loading_overlay.dart
@@ -4,6 +4,44 @@ import 'package:clerk_flutter/src/utils/clerk_auth_config.dart';
 import 'package:clerk_flutter/src/widgets/ui/common.dart';
 import 'package:flutter/widgets.dart';
 
+/// Abstract class proxying [OverlayState]
+abstract class AbstractClerkOverlayState {
+  /// Insert an [OverlayEntry]
+  void insert(OverlayEntry entry);
+
+  /// Remove an [OverlayEntry]
+  void remove(OverlayEntry entry);
+
+  /// Is the [OverlayEntry] on display?
+  bool isDisplaying(OverlayEntry entry);
+
+  /// Is this [AbstractClerkOverlayState] mounted?
+  bool get mounted;
+}
+
+/// UI instantiation of [AbstractClerkOverlayState]
+class ClerkOverlay extends AbstractClerkOverlayState {
+  /// Construct a [ClerkOverlay]
+  ClerkOverlay._(this._overlay);
+
+  final OverlayState _overlay;
+
+  @override
+  void insert(OverlayEntry entry) => _overlay.insert(entry);
+
+  @override
+  void remove(OverlayEntry entry) => entry.remove();
+
+  @override
+  bool isDisplaying(OverlayEntry entry) => entry.mounted;
+
+  @override
+  bool get mounted => _overlay.context.mounted;
+
+  /// Find the [ClerkOverlay]
+  static of(BuildContext context) => ClerkOverlay._(Overlay.of(context));
+}
+
 /// Clerk Loading Overlay
 class ClerkLoadingOverlay {
   /// Constructs a [ClerkLoadingOverlay]
@@ -12,52 +50,54 @@ class ClerkLoadingOverlay {
           builder: (context) => config.loading ?? defaultLoadingWidget,
         );
 
-  static const _startupDuration = Duration(milliseconds: 300);
-  static const _minimumOnScreenDuration = Duration(milliseconds: 500);
+  /// The delay between an [insertInto] call and the loading overlay
+  /// being displayed
+  static const startupDuration = Duration(milliseconds: 300);
 
-  Timer? _startupTimer;
-  Timer? _cancelTimer;
-  DateTime _cancelAfter = DateTime(0);
+  /// The minimum amount of time the loading overlay should remain
+  /// on screen for
+  static const minimumOnScreenDuration = Duration(milliseconds: 500);
+
+  int _count = 0;
+  Timer? _timer;
+  DateTime _hideAfter = DateTime(0);
 
   final OverlayEntry _overlayEntry;
 
   /// Shows the loading overlay
-  void insertInto(OverlayState overlay) {
-    _cancelTimer?.cancel();
-    _cancelTimer = null;
-    if (_overlayEntry.mounted == false && _startupTimer == null) {
-      _startupTimer = Timer(
-        _startupDuration,
-        () {
-          if (overlay.context.mounted) {
-            overlay.insert(_overlayEntry);
-            _cancelAfter = DateTime.timestamp().add(_minimumOnScreenDuration);
-          }
-        },
-      );
-    }
+  void insertInto(AbstractClerkOverlayState overlay) {
+    _count++;
+    _timer ??= Timer(
+      startupDuration,
+      () {
+        if (overlay.mounted && overlay.isDisplaying(_overlayEntry) == false) {
+          _hideAfter = DateTime.timestamp().add(minimumOnScreenDuration);
+          overlay.insert(_overlayEntry);
+        }
+      },
+    );
   }
 
   /// Hides the loading overlay
-  void remove() {
-    _startupTimer?.cancel();
-    _startupTimer == null;
+  void removeFrom(AbstractClerkOverlayState overlay) {
+    _count--;
+    if (_count == 0) {
+      _timer?.cancel();
+      _timer = null;
 
-    _cancelTimer?.cancel();
-    _cancelTimer = null;
-
-    if (_overlayEntry.mounted) {
-      final now = DateTime.timestamp();
-      if (_cancelAfter.isAfter(now)) {
-        _cancelTimer = Timer(
-          _cancelAfter.difference(now),
-          () {
-            _cancelTimer = null;
-            _overlayEntry.remove();
-          },
-        );
-      } else {
-        _overlayEntry.remove();
+      if (overlay.isDisplaying(_overlayEntry)) {
+        final now = DateTime.timestamp();
+        if (_hideAfter.isAfter(now)) {
+          _timer = Timer(
+            _hideAfter.difference(now),
+            () {
+              _timer = null;
+              overlay.remove(_overlayEntry);
+            },
+          );
+        } else {
+          overlay.remove(_overlayEntry);
+        }
       }
     }
   }

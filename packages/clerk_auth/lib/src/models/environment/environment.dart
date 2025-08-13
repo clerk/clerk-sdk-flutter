041502f637638f7d92092c2808db2163af2be445
Nic Ford <nicford@devangels.london>
2025-08-13 19:59:26 +0100

    fix: reformat [#207]

--- packages/clerk_auth/lib/src/models/environment/environment.dart
+++ packages/clerk_auth/lib/src/models/environment/environment.dart
@@ -51,14 +51,19 @@ class Environment with InformativeToStringMixin {
   static const empty = Environment();
 
   /// Do we have [Strategy.password] configured?
-  bool get hasPasswordStrategy => config.firstFactors.contains(Strategy.password);
+  bool get hasPasswordStrategy =>
+      config.firstFactors.contains(Strategy.password);
 
   /// [List] of identification strategies
   List<Strategy> get strategies => config.identificationStrategies;
 
   /// [Iterable] of non-oauth and non-phone identification strategies
   Iterable<Strategy> get identificationStrategies => strategies.where(
-        (i) => const [Strategy.emailAddress, Strategy.username, Strategy.phoneNumber].contains(i),
+        (i) => const [
+          Strategy.emailAddress,
+          Strategy.username,
+          Strategy.phoneNumber
+        ].contains(i),
       );
 
   /// Do we have identification strategies?
@@ -72,13 +77,15 @@ class Environment with InformativeToStringMixin {
 
   /// [Iterable] of other strategies
   /// i.e. strategies that are neither oauth nor password-based
-  Iterable<Strategy> get otherStrategies => strategies.where((f) => f.isOtherStrategy);
+  Iterable<Strategy> get otherStrategies =>
+      strategies.where((f) => f.isOtherStrategy);
 
   /// Do we have other strategies?
   bool get hasOtherStrategies => otherStrategies.isNotEmpty;
 
   /// fromJson
-  static Environment fromJson(Map<String, dynamic> json) => _$EnvironmentFromJson(json);
+  static Environment fromJson(Map<String, dynamic> json) =>
+      _$EnvironmentFromJson(json);
 
   /// toJson
   @override

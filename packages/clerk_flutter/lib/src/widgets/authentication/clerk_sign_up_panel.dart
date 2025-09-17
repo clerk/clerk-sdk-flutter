03bf379665f4ec14856bd9ac0005af1a1e281fc9
Nic Ford <nicford@devangels.london>
2025-09-17 10:49:07 +0100

    feat: bring sign up ux in line with other sdks [#246]

--- packages/clerk_flutter/lib/src/widgets/authentication/clerk_sign_up_panel.dart
+++ packages/clerk_flutter/lib/src/widgets/authentication/clerk_sign_up_panel.dart
@@ -9,7 +9,9 @@ import 'package:clerk_flutter/src/widgets/ui/clerk_phone_number_form_field.dart'
 import 'package:clerk_flutter/src/widgets/ui/clerk_text_form_field.dart';
 import 'package:clerk_flutter/src/widgets/ui/closeable.dart';
 import 'package:clerk_flutter/src/widgets/ui/common.dart';
+import 'package:clerk_flutter/src/widgets/ui/style/colors.dart';
 import 'package:clerk_flutter/src/widgets/ui/style/text_style.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:phone_input/phone_input_package.dart';
 import 'package:url_launcher/url_launcher_string.dart';
@@ -54,11 +56,11 @@ class _ClerkSignUpPanelState extends State<ClerkSignUpPanel>
   bool _highlightMissing = false;
 
   static const _signUpAttributes = [
+    clerk.UserAttribute.firstName,
+    clerk.UserAttribute.lastName,
     clerk.UserAttribute.username,
     clerk.UserAttribute.emailAddress,
     clerk.UserAttribute.phoneNumber,
-    clerk.UserAttribute.firstName,
-    clerk.UserAttribute.lastName,
     clerk.UserAttribute.password,
   ];
 
@@ -166,21 +168,14 @@ class _ClerkSignUpPanelState extends State<ClerkSignUpPanel>
 
   void _onObscure() => setState(() => _isObscured = !_isObscured);
 
-  void _acceptTerms() => setState(() => _hasLegalAcceptance = true);
+  void _acceptTerms() =>
+      setState(() => _hasLegalAcceptance = !_hasLegalAcceptance);
 
   void _reset() => setState(() => _state = _SignUpPanelState.input);
 
   _ValueChanger _change(clerk.UserAttribute attr) => (String value) {
         _values[attr] = value;
       };
-
-  Widget _link(String label, String url) {
-    return GestureDetector(
-      behavior: HitTestBehavior.opaque,
-      onTap: () => launchUrlString(url),
-      child: Text(label, style: ClerkTextStyle.clickable),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -198,6 +193,9 @@ class _ClerkSignUpPanelState extends State<ClerkSignUpPanel>
             case clerk.UserAttributeData data when data.isEnabled) //
           _Attribute(attr, data),
     ];
+    final lastNameAttr = attributes.any((a) => a.isFirstName)
+        ? attributes.removeFirstOrNull((a) => a.isLastName)
+        : null;
     final isAwaitingCode = (env.supportsEmailCode &&
             signUp?.unverified(clerk.Field.emailAddress) == true) ||
         (env.supportsPhoneCode &&
@@ -269,6 +267,33 @@ class _ClerkSignUpPanelState extends State<ClerkSignUpPanel>
                     isOptional: attribute.isOptional,
                     onChanged: _change(clerk.UserAttribute.phoneNumber),
                   )
+                else if (attribute.isFirstName) //
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        child: ClerkTextFormField(
+                          initial: _values[attribute.attr],
+                          label: attribute.title(l10ns),
+                          isMissing: isMissing(attribute),
+                          isOptional: attribute.isOptional,
+                          onChanged: _change(attribute.attr),
+                        ),
+                      ),
+                      if (lastNameAttr case final lastNameAttr?) ...[
+                        horizontalMargin16,
+                        Expanded(
+                          child: ClerkTextFormField(
+                            initial: _values[lastNameAttr.attr],
+                            label: lastNameAttr.title(l10ns),
+                            isMissing: isMissing(lastNameAttr),
+                            isOptional: lastNameAttr.isOptional,
+                            onChanged: _change(lastNameAttr.attr),
+                          ),
+                        ),
+                      ],
+                    ],
+                  )
                 else if (attribute.isPassword) ...[
                   ClerkTextFormField(
                     initial: _values[clerk.UserAttribute.password],
@@ -306,48 +331,97 @@ class _ClerkSignUpPanelState extends State<ClerkSignUpPanel>
         Closeable(
           closed: (_state.isWaiting && isAwaitingCode == false) ||
               (_needsLegalAcceptance && _hasLegalAcceptance == false),
-          child: ClerkMaterialButton(
-            onPressed: () => _continue(attributes),
-            label: Row(
-              children: [
-                horizontalMargin16,
-                Expanded(child: Center(child: Text(l10ns.cont))),
-                const SizedBox(
-                  width: 16,
-                  child: Icon(Icons.arrow_right_sharp),
-                ),
-              ],
-            ),
-          ),
-        ),
-        if (_needsLegalAcceptance) //
-          Closeable(
-            closed: _hasLegalAcceptance,
-            child: Padding(
-              padding: topPadding4,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Padding(
+            padding: bottomPadding16,
+            child: ClerkMaterialButton(
+              onPressed: () => _continue(attributes),
+              label: Row(
                 children: [
-                  ClerkMaterialButton(
-                    onPressed: _acceptTerms,
-                    label: Text(l10ns.acceptTerms),
-                  ),
-                  verticalMargin4,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      if (env.display.termsUrl case String termsUrl)
-                        _link(l10ns.termsAndConditions, termsUrl),
-                      if (env.display.privacyPolicyUrl case String privacyUrl)
-                        _link(l10ns.privacyPolicy, privacyUrl),
-                    ],
+                  horizontalMargin16,
+                  Expanded(child: Center(child: Text(l10ns.cont))),
+                  const SizedBox(
+                    width: 16,
+                    child: Icon(Icons.arrow_right_sharp),
                   ),
                 ],
               ),
             ),
           ),
+        ),
+        if (_needsLegalAcceptance) //
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: _acceptTerms,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 8, 8),
+                  child: Icon(
+                    _hasLegalAcceptance
+                        ? Icons.check_box_outlined
+                        : Icons.check_box_outline_blank,
+                  ),
+                ),
+              ),
+              const Expanded(child: _LegalAcceptanceConfirmation()),
+            ],
+          ),
         verticalMargin32,
       ],
+    );
+  }
+}
+
+class _LegalAcceptanceConfirmation extends StatelessWidget {
+  const _LegalAcceptanceConfirmation();
+
+  List<TextSpan> _subSpans(String text, String target, String? url) {
+    if (url case String url) {
+      final segments = text.split(target);
+      final spans = <TextSpan>[TextSpan(text: segments.first)];
+
+      for (final segmentText in segments.skip(1)) {
+        spans.add(
+          TextSpan(
+            text: target,
+            style: const TextStyle(color: ClerkColors.azure),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () => launchUrlString(url),
+          ),
+        );
+        spans.add(TextSpan(text: segmentText));
+      }
+
+      return spans;
+    }
+
+    return [TextSpan(text: text)];
+  }
+
+  List<InlineSpan> _spans(BuildContext context) {
+    final authState = ClerkAuth.of(context, listen: false);
+    final display = authState.env.display;
+    final l10ns = authState.localizationsOf(context);
+    final spans =
+        _subSpans(l10ns.acceptTerms, l10ns.termsOfService, display.termsUrl);
+
+    return [
+      for (final span in spans) //
+        if (span.text case String text when span.recognizer == null) //
+          ..._subSpans(text, l10ns.privacyPolicy, display.privacyPolicyUrl)
+        else //
+          span,
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(
+      TextSpan(children: _spans(context)),
+      maxLines: 2,
+      style: ClerkTextStyle.subtitleDark,
     );
   }
 }
@@ -442,6 +516,10 @@ class _Attribute {
   bool get isPhoneNumber => attr == clerk.UserAttribute.phoneNumber;
 
   bool get isPassword => attr == clerk.UserAttribute.password;
+
+  bool get isFirstName => attr == clerk.UserAttribute.firstName;
+
+  bool get isLastName => attr == clerk.UserAttribute.lastName;
 
   bool get isRequired => data.isRequired;
 

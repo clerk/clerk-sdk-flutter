fd81c582874942dc15c8edbd1aac4182e2d60979
Nic Ford <nicford@devangels.london>
2025-10-20 18:06:13 +0100

    fix: make email link test work [#286]

--- packages/clerk_auth/test/integration/clerk_auth/sign_up_test.dart
+++ packages/clerk_auth/test/integration/clerk_auth/sign_up_test.dart
@@ -118,63 +118,52 @@ void main() {
       });
     });
 
-    test('does not updateSignUp when values are unchanged', () async {
+    test('does not update SignUp when values are unchanged', () async {
       await runWithLogging(() async {
-        expect(auth.user, null);
+        await initialiseForTest('does_not_update_unchanged_values');
+
+        const email = 'newemail+clerk_test@somedomain.com';
 
         // 1) Create initial sign up with emailCode and an email address
-        httpService.expect(
-          'POST /v1/client/sign_ups strategy=email_code&email_address=$emailAddress',
-          200,
-          '{"response":{"object":"sign_up_attempt","id":"SIGN_UP_ATTEMPT_ID","status":"missing_requirements","required_fields":["password"],"optional_fields":["oauth_github","oauth_google","email_address","phone_number","username","last_name","first_name","oauth_apple"],"missing_fields":["password"],"unverified_fields":["email_address"],"verifications":{"email_address":null,"phone_number":null,"web3_wallet":null,"external_account":null},"username":null,"email_address":"$emailAddress","phone_number":null,"web3_wallet":null,"password_enabled":false,"first_name":null,"last_name":null,"unsafe_metadata":{},"public_metadata":{},"custom_action":false,"external_id":null,"created_session_id":null,"created_user_id":null,"abandon_at":1732107014735,"legal_accepted_at":null},"client":{"object":"client","id":"CLIENT_ID","sessions":[],"sign_in":null,"sign_up":{"object":"sign_up_attempt","id":"SIGN_UP_ATTEMPT_ID","status":"missing_requirements","required_fields":["password"],"optional_fields":["phone_number","username","first_name","last_name","oauth_github","oauth_google","oauth_apple","email_address"],"missing_fields":["password"],"unverified_fields":["email_address"],"verifications":{"email_address":null,"phone_number":null,"web3_wallet":null,"external_account":null},"username":null,"email_address":"$emailAddress","phone_number":null,"web3_wallet":null,"password_enabled":false,"first_name":null,"last_name":null,"unsafe_metadata":{},"public_metadata":{},"custom_action":false,"external_id":null,"created_session_id":null,"created_user_id":null,"abandon_at":1732107014735,"legal_accepted_at":null},"last_active_session_id":null,"cookie_expires_at":null,"created_at":1732020614717,"updated_at":1732020614748}}',
-        );
-
         // Initial create
         Client client = await auth.attemptSignUp(
           strategy: Strategy.emailCode,
-          emailAddress: emailAddress,
+          emailAddress: email,
         );
         expect(client.signUp?.status, Status.missingRequirements);
+        final hitCount = httpService.hitCount;
 
         // 2) Call attemptSignUp again with the same email
         // No HTTP expectation should be queued for this invocation.
         client = await auth.attemptSignUp(
           strategy: Strategy.emailCode,
-          emailAddress: emailAddress, // unchanged
+          emailAddress: email, // unchanged
         );
 
         // State should remain in missing requirements; no extra PATCH occurred.
         expect(client.signUp?.status, Status.missingRequirements);
+
+        // hit count should not have changed
+        expect(httpService.hitCount, hitCount);
       });
     });
 
     test('updates SignUp when provided value changes', () async {
       await runWithLogging(() async {
-        expect(auth.user, null);
+        await initialiseForTest('does_update_changed_values');
 
-        final firstEmail = '$username+first@some.domain';
-        final secondEmail = '$username+second@some.domain';
+        final firstEmail = '${env.username}+first@somedomain.com';
+        final secondEmail = '${env.username}+second@somedomain.com';
 
         // 1) Create initial sign up with first email
-        httpService.expect(
-          'POST /v1/client/sign_ups strategy=email_code&email_address=$firstEmail',
-          200,
-          '{"response":{"object":"sign_up_attempt","id":"SIGN_UP_ATTEMPT_ID","status":"missing_requirements","required_fields":["password"],"optional_fields":["oauth_github","oauth_google","email_address","phone_number","username","last_name","first_name","oauth_apple"],"missing_fields":["password"],"unverified_fields":["email_address"],"verifications":{"email_address":null,"phone_number":null,"web3_wallet":null,"external_account":null},"username":null,"email_address":"$firstEmail","phone_number":null,"web3_wallet":null,"password_enabled":false,"first_name":null,"last_name":null,"unsafe_metadata":{},"public_metadata":{},"custom_action":false,"external_id":null,"created_session_id":null,"created_user_id":null,"abandon_at":1732107014735,"legal_accepted_at":null},"client":{"object":"client","id":"CLIENT_ID","sessions":[],"sign_in":null,"sign_up":{"object":"sign_up_attempt","id":"SIGN_UP_ATTEMPT_ID","status":"missing_requirements","required_fields":["password"],"optional_fields":["phone_number","username","first_name","last_name","oauth_github","oauth_google","oauth_apple","email_address"],"missing_fields":["password"],"unverified_fields":["email_address"],"verifications":{"email_address":null,"phone_number":null,"web3_wallet":null,"external_account":null},"username":null,"email_address":"$firstEmail","phone_number":null,"web3_wallet":null,"password_enabled":false,"first_name":null,"last_name":null,"unsafe_metadata":{},"public_metadata":{},"custom_action":false,"external_id":null,"created_session_id":null,"created_user_id":null,"abandon_at":1732107014735,"legal_accepted_at":null},"last_active_session_id":null,"cookie_expires_at":null,"created_at":1732020614717,"updated_at":1732020614748}}',
-        );
-
         Client client = await auth.attemptSignUp(
           strategy: Strategy.emailCode,
           emailAddress: firstEmail,
         );
         expect(client.signUp?.status, Status.missingRequirements);
+        final hitCount = httpService.hitCount;
 
         // 2) Provide a different email -> should PATCH
-        httpService.expect(
-          'PATCH /v1/client/sign_ups/SIGN_UP_ATTEMPT_ID strategy=email_code&email_address=$secondEmail',
-          200,
-          '{"response":{"object":"sign_up_attempt","id":"SIGN_UP_ATTEMPT_ID","status":"missing_requirements","required_fields":["password"],"optional_fields":["oauth_github","oauth_google","email_address","phone_number","username","last_name","first_name","oauth_apple"],"missing_fields":["password"],"unverified_fields":["email_address"],"verifications":{"email_address":null,"phone_number":null,"web3_wallet":null,"external_account":null},"username":null,"email_address":"$secondEmail","phone_number":null,"web3_wallet":null,"password_enabled":false,"first_name":null,"last_name":null,"unsafe_metadata":{},"public_metadata":{},"custom_action":false,"external_id":null,"created_session_id":null,"created_user_id":null,"abandon_at":1732107014735,"legal_accepted_at":null},"client":{"object":"client","id":"CLIENT_ID","sessions":[],"sign_in":null,"sign_up":{"object":"sign_up_attempt","id":"SIGN_UP_ATTEMPT_ID","status":"missing_requirements","required_fields":["password"],"optional_fields":["phone_number","username","first_name","last_name","oauth_github","oauth_google","oauth_apple","email_address"],"missing_fields":["password"],"unverified_fields":["email_address"],"verifications":{"email_address":null,"phone_number":null,"web3_wallet":null,"external_account":null},"username":null,"email_address":"$secondEmail","phone_number":null,"web3_wallet":null,"password_enabled":false,"first_name":null,"last_name":null,"unsafe_metadata":{},"public_metadata":{},"custom_action":false,"external_id":null,"created_session_id":null,"created_user_id":null,"abandon_at":1732107014735,"legal_accepted_at":null},"last_active_session_id":null,"cookie_expires_at":null,"created_at":1732020614717,"updated_at":1732020614748}}',
-        );
-
         client = await auth.attemptSignUp(
           strategy: Strategy.emailCode,
           emailAddress: secondEmail,
@@ -183,6 +172,9 @@ void main() {
         // Still in missing requirements, but email reflected the change via PATCH
         expect(client.signUp?.status, Status.missingRequirements);
         expect(client.signUp?.emailAddress, secondEmail);
+
+        // The hit count should have gone up by one
+        expect(httpService.hitCount, hitCount + 1);
       });
     });
   });
